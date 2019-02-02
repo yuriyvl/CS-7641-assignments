1ee3222f47908956af949e5f40ac5a08b5a5c287
Yuriy Lichman <yuriy_lichman@intuit.com>
2019-02-01 19:51:42 -0800

    Using SVMLearner, added best params

--- assignment1/experiments/SVM.py
+++ assignment1/experiments/SVM.py
@@ -17,10 +17,10 @@ class SVMExperiment(experiments.BaseExperiment):
         samples = self._details.ds.features.shape[0]
         features = self._details.ds.features.shape[1]
 
-        gamma_fracs = np.arange(1/features, 2.1, 0.2)
+        gamma_fracs = np.arange(1 / features, 2.1, 0.2)
         tols = np.arange(1e-8, 1e-1, 0.01)
         C_values = np.arange(0.001, 2.5, 0.25)
-        iters = [-1, int((1e6/samples)/.8)+1]
+        iters = [-1, int((1e6 / samples) / .8) + 1]
 
         best_params_linear = None
         best_params_rbf = None
@@ -38,62 +38,126 @@ class SVMExperiment(experiments.BaseExperiment):
         # best_params_rbf = {'C': 1.5, 'class_weight': 'balanced', 'decision_function_shape': 'ovo',
         #                    'gamma': 0.125, 'max_iter': -1, 'tol': 0.07000001}
 
-        # Linear SVM
-        params = {'SVM__max_iter': iters, 'SVM__tol': tols, 'SVM__class_weight': ['balanced'],
-                  'SVM__C': C_values}
-        complexity_param = {'name': 'SVM__C', 'display_name': 'Penalty', 'values': np.arange(0.001, 2.5, 0.1)}
+        # Adult:
+        # best_params_linear = {
+        #     'C': 0.251,
+        #     'cache_size': 200,
+        #     'class_weight': 'balanced',
+        #     'coef0': 0.0,
+        #     'decision_function_shape': 'ovo',
+        #     'degree': 3,
+        #     'gamma': 0.017857142857142856,
+        #     'kernel': 'linear',
+        #     'max_iter': -1,
+        #     'probability': False,
+        #     'shrinking': True,
+        #     'tol': 0.08000001,
+        #     'verbose': False
+        # }
+        #
+        # best_params_rbf = {
+        #     'C': 1.251,
+        #     'cache_size': 200,
+        #     'class_weight': 'balanced',
+        #     'coef0': 0.0,
+        #     'decision_function_shape': 'ovo',
+        #     'degree': 3,
+        #     'gamma': 0.017857142857142856,
+        #     'kernel': 'rbf',
+        #     'max_iter': -1,
+        #     'probability': False,
+        #     'shrinking': True,
+        #     'tol': 0.06000001,
+        #     'verbose': False
+        # }
+        #
+        # Spam:
+        #
+        # best_params_linear = {
+        #     'C': 2.251,
+        #     'cache_size': 200,
+        #     'class_weight': 'balanced',
+        #     'coef0': 0.0,
+        #     'decision_function_shape': 'ovo',
+        #     'degree': 3,
+        #     'gamma': 0.017543859649122806,
+        #     'kernel': 'linear',
+        #     'max_iter': 1,
+        #     'probability': False,
+        #     'shrinking': True,
+        #     'tol': 0.09000000999999999,
+        #     'verbose': False
+        # }
 
-        iteration_details = {
-            'x_scale': 'log',
-            'params': {'SVM__max_iter': [2**x for x in range(12)]},
-        }
+        # best_params_rbf = {
+        #     'C': 2.001,
+        #     'cache_size': 200,
+        #     'class_weight': 'balanced',
+        #     'coef0': 0.0,
+        #     'decision_function_shape': 'ovo',
+        #     'degree': 3,
+        #     'gamma': 0.017543859649122806,
+        #     'kernel': 'rbf',
+        #     'max_iter': 1,
+        #     'probability': False,
+        #     'shrinking': True,
+        #     'tol': 0.03000001,
+        #     'verbose': False
+        # }
 
-        # NOTE: If this is causing issues, try the RBFSVMLearner. Passing use_linear=True will use a linear kernel
-        #       and passing use_linear=False will use the RBF kernel. This method is slower but if libsvm is not
-        #       available it may be your only option
-        learner = learners.LinearSVMLearner(dual=False)
-        if best_params_linear is not None:
-            learner.set_params(**best_params_linear)
-
-        best_params = experiments.perform_experiment(
-            self._details.ds, self._details.ds_name, self._details.ds_readable_name, learner, 'SVMLinear', 'SVM',
-            params, complexity_param=complexity_param, seed=self._details.seed, iteration_details=iteration_details,
-            best_params=best_params_linear,
-            threads=self._details.threads, verbose=self._verbose)
-
-        of_params = best_params.copy()
-        learner = learners.LinearSVMLearner(dual=True)
-        if best_params_linear is not None:
-            learner.set_params(**best_params_linear)
-        experiments.perform_experiment(self._details.ds, self._details.ds_name, self._details.ds_readable_name, learner,
-                                       'SVMLinear_OF', 'SVM', of_params, seed=self._details.seed,
-                                       iteration_details=iteration_details,
-                                       best_params=best_params_linear,
-                                       threads=self._details.threads, verbose=self._verbose,
-                                       iteration_lc_only=True)
-
-        # RBF SVM
+        ########## RBF SVM
         params = {'SVM__max_iter': iters, 'SVM__tol': tols, 'SVM__class_weight': ['balanced'],
                   'SVM__C': C_values,
                   'SVM__decision_function_shape': ['ovo', 'ovr'], 'SVM__gamma': gamma_fracs}
-        complexity_param = {'name': 'SVM__C', 'display_name': 'Penalty', 'values': np.arange(0.001, 2.5, 0.1)}
+        # complexity_param = {'name': 'SVM__C', 'display_name': 'Penalty', 'values': np.arange(0.001, 2.5, 0.1)}
+        complexity_param = {'name': 'SVM__gamma', 'display_name': 'Gamma', 'values': np.arange(1 / features, 2.1, 0.2)}
+
+        iteration_details = {
+            # 'x_scale': 'log',
+            'params': {'SVM__max_iter': [2 ** x for x in range(12)]},
+        }
 
         learner = learners.SVMLearner(kernel='rbf')
         if best_params_rbf is not None:
             learner.set_params(**best_params_rbf)
+
         best_params = experiments.perform_experiment(
             self._details.ds, self._details.ds_name, self._details.ds_readable_name, learner, 'SVM_RBF', 'SVM',
             params, complexity_param=complexity_param, seed=self._details.seed, iteration_details=iteration_details,
             best_params=best_params_rbf,
             threads=self._details.threads, verbose=self._verbose)
 
-        of_params = best_params.copy()
-        learner = learners.SVMLearner(kernel='rbf')
-        if best_params_rbf is not None:
-            learner.set_params(**best_params_rbf)
-        experiments.perform_experiment(self._details.ds, self._details.ds_name, self._details.ds_readable_name, learner,
-                                       'SVM_RBF_OF', 'SVM', of_params, seed=self._details.seed,
-                                       iteration_details=iteration_details,
-                                       best_params=best_params_rbf,
-                                       threads=self._details.threads, verbose=self._verbose,
-                                       iteration_lc_only=True)
+        # Overfitting - currently doesn't seem to do anything useful...
+
+        # of_params = best_params.copy()
+        # learner = learners.SVMLearner(kernel='rbf')
+        # if best_params_rbf is not None:
+        #     learner.set_params(**best_params_rbf)
+        # experiments.perform_experiment(self._details.ds, self._details.ds_name, self._details.ds_readable_name, learner,
+        #                                'SVM_RBF_OF', 'SVM', of_params, seed=self._details.seed,
+        #                                iteration_details=iteration_details,
+        #                                best_params=best_params_rbf,
+        #                                threads=self._details.threads, verbose=self._verbose,
+        #                                iteration_lc_only=True)
+
+        ########## LINEAR SVM
+        learner = learners.SVMLearner(kernel='linear')
+        if best_params_linear is not None:
+            learner.set_params(**best_params_linear)
+
+        best_params = experiments.perform_experiment(
+            self._details.ds, self._details.ds_name, self._details.ds_readable_name, learner, 'SVM_LINEAR', 'SVM',
+            params, complexity_param=complexity_param, seed=self._details.seed, iteration_details=iteration_details,
+            best_params=best_params_rbf,
+            threads=self._details.threads, verbose=self._verbose)
+
+        # of_params = best_params.copy()
+        # learner = learners.SVMLearner(kernel='linear')
+        # if best_params_rbf is not None:
+        #     learner.set_params(**best_params_rbf)
+        # experiments.perform_experiment(self._details.ds, self._details.ds_name, self._details.ds_readable_name, learner,
+        #                                'SVM_LINEAR_OF', 'SVM', of_params, seed=self._details.seed,
+        #                                iteration_details=iteration_details,
+        #                                best_params=best_params_rbf,
+        #                                threads=self._details.threads, verbose=self._verbose,
+        #                                iteration_lc_only=True)
